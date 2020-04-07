d9f92e5eabc0e33c4110434a5f1514992b128368
Reinelin <yclinreine@gmail.com>
2020-04-07 21:21:59 +0800

    add count

--- guess.py
+++ guess.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1
 	num = input ('please enter a number:')
 	num = int(num)
 	if num == r:
 		print('correct!')
+		print ('this is your', count, 'time(s)')
 		break
 	elif num > r:
 		print('enter smaller')
 	elif num < r:
 		print('enter larger')
+	print ('this is your', count, 'time(s)')
